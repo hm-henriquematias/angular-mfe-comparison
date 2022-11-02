51f304860b410225055329d8375a627ce28a330b
Henrique Matias <henriquematiasdesouza@gmail.com>
2022-11-01 21:06:40 -0300

    feature: Update root app.module.ts
    - Add LazyElementsModule and Commom Module on imports
    - Add schema CUSTOM_ELEMENTS_SCHEMA

--- remote-serving/mfe-root/src/app/app.module.ts
+++ remote-serving/mfe-root/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { LazyElementsModule } from '@angular-extensions/elements/public_api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    LazyElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
